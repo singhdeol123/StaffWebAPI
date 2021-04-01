8f96b994d42a66e8ea770f5a21e1e99ad339b706
Hp <Hp@DESKTOP-MFDAATE>
2021-04-01 13:43:56 +1300

    Add project files.

---
+++ Data/StaffWebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StaffWebAPI.Models;
+
+namespace StaffWebAPI.Data
+{
+    public class StaffWebAPIContext : DbContext
+    {
+        public StaffWebAPIContext (DbContextOptions<StaffWebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StaffWebAPI.Models.StaffName> StaffName { get; set; }
+    }
+}
